4edb37c323095c66e496878fe867b650ee122195
Gino Stoian <ginostoian@gmail.com>
2023-12-26 12:43:18 +0200

    add building block components

--- app/page.js
+++ app/page.js
@@ -5,6 +5,12 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import FeaturesAccordion from "@/components/FeaturesAccordion";
 import FeaturesListicle from "@/components/FeaturesListicle";
+import Testimonials3 from "@/components/Testimonials3";
+import Pricing from "@/components/Pricing";
+import FAQ from "@/components/FAQ";
+import Testimonials11 from "@/components/Testimonials11";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
 
 export default function Page() {
   return (
@@ -14,6 +20,12 @@ export default function Page() {
         <Hero />
         <FeaturesListicle />
         <FeaturesAccordion />
+        <Testimonials3 />
+        <Pricing />
+        <FAQ />
+        <Testimonials11 />
+        <CTA />
+        <Footer />
       </main>
     </>
   );
